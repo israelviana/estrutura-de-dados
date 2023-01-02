514d05acf8227193df5bd11914dcf9c178d207f1
israelviana <rael232is@gmail.com>
2023-01-02 20:07:57 -0300

    ajuste menu

--- src/Menu.java
+++ src/Menu.java
@@ -18,8 +18,8 @@ public class Menu {
             case 1:
                 System.out.println("Digite qual método deseja executar!");
                 System.out.println("1 - Vetor de Inteiros\n2 - Vetor de String\n3 - Busca Linear");
-                int metodo = sc.nextInt();
-                switch (metodo){
+                int metodo1 = sc.nextInt();
+                switch (metodo1){
                     case 1:
                         v.RunVetorInteiros();
                     case 2:
@@ -29,7 +29,21 @@ public class Menu {
                 }
                 break;
             case 2:
-                System.out.println(lista.getTamanho());
+                System.out.println("Digite qual método deseja executar!");
+                System.out.println("1 - Adicionar elementos na lista\n2 - Remover elementos da lista\n3 - Buscar elemento específico através da posição");
+                int metodo2 = sc.nextInt();
+                switch (metodo2){
+                    case 1:
+                        lista.adicionar("AC");
+                        lista.adicionar("CE");
+                        lista.adicionar("BA");
+                        System.out.println("Tamanho: " + lista.getTamanho());
+                        System.out.println("Primeiro: " + lista.getPrimeiro().getValor());
+                        System.out.println("Ultimo: " + lista.getUltimo().getValor());
+                        System.out.println(lista.get(0).getValor());
+                        System.out.println(lista.get(1).getValor());
+                        System.out.println(lista.get(2).getValor());
+                }
         }
     }
 }
